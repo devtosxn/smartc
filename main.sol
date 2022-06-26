588ae2e43abc585110033753c2459967dd73dbd5
Tosin Ayodele <tosinayodele@Tosins-MacBook-Pro.local>
2022-06-26 21:11:28 +0100

    add simple contract to store name and retrieve name

--- main.sol
+++ main.sol
@@ -1,4 +1,11 @@
 // Write a simple smart contract that stores your name and also includes a function to retrieve your name.
 
+pragma solidity ^0.8.15;
 
+contract MyContract {
+    string name;
 
+    function getName() public view returns (string memory) {
+        return name;
+    }
+}
